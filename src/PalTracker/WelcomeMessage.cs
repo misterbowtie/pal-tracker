8f8fcb9991e72950cdfb78ccd656d25a1ae7f883
Joel Leach & Igor Livitz <jleach5@humana.com&ilivitz@humana.com>
2019-03-11 13:10:10 -0600

    Added test and manifest file

---
+++ src/PalTracker/WelcomeMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalTracker
+{
+    public class WelcomeMessage
+    {
+        public string Message { get;  }
+
+        public WelcomeMessage (string message)
+        {
+            Message = message;
+        }
+
+    }
+}
